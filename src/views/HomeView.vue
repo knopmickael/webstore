d1571b5ce5ddca3655f40da835280122632c837e
Mickael Knop <knopmickael@gmail.com>
2023-02-08 17:45:55 -0300

    project scaffold up

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <HomeCarousel />
+    <HomeStore />
+    <HomeAbout />
+  </div>
+</template>
+
+<script setup>
+import HomeCarousel from '../components/Home/Carousel.vue'
+import HomeStore from '../components/Home/Store.vue'
+import HomeAbout from '../components/Home/About.vue'
+</script>
