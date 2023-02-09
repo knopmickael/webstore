690ae3e198eb7296dd8fc5d9fdf6d4c805b4b8c9
Mickael Knop <knopmickael@gmail.com>
2023-02-08 22:56:18 -0300

    components style upd

--- src/components/Home/Store.vue
+++ src/components/Home/Store.vue
@@ -35,12 +35,12 @@
         <p>Aguardando produtos do servidor...</p>
       </div>
       <div v-else class="container mx-auto flex items-center flex-wrap">
-        <div v-for="product in products" :key="product.id" class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-          <RouterLink :to="`/products/${product.id}`">
-            <img class="hover:grow hover:shadow-lg"
+        <div v-for="product in products" :key="product.id" class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col text-center">
+          <RouterLink :to="`/products/${product.id}`" class="space-y-4">
+            <img class="hover:grow hover:opacity-70 max-h-56 mx-auto"
               :src="product.image">
             <div class="pt-3 flex items-center justify-between">
-              <p class="">{{ product.title }}</p>
+              <p class="font-bold mx-auto">{{ product.title }}</p>
             </div>
             <p class="pt-1 text-gray-900">$ {{ product.price }}</p>
           </RouterLink>
