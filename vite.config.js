50ef39a2d3a3d32db387dedddfa0e12b47310b5f
Mickael Knop <knopmickael@gmail.com>
2023-02-12 20:09:24 -0300

    upd vite env vars

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  env: {
+    BASE_URL: process.env.NODE_ENV === 'production'
+      ? '/webstore/'
+      : '/'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
